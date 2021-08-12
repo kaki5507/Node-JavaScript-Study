85636bf188d1da87b5a3e98401a0e8bc01fb3206
kaki <ckdbioksh@naver.com>
2021-08-12 09:17:44 +0900

    module create
    - add,mult 모듈을 만들어서 app.js에서 불러와서 사용해보았다.

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Console test");
+const stat = require('./stat');
+
+console.log(stat.pi);
+console.log(stat.mean([1,2,3,4,5]));
---
+++ mult.js
@@ -0,0 +1,8 @@
+/*
+const mult = (a, b) => {
+    return a * b;
+};
+
+module.exports = mult;*/
+/* --------------------- */
+module.exports = (a, b) => a * b;
